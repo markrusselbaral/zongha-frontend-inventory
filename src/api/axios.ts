d03bf30af3e3d3554f537b5ca3907391a25b2e91
markrusselbaral <global-mark.baral@acacia-soft.com>
2024-02-14 23:09:28 +0800

    fix

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL : "http://3.26.10.163",
+    baseURL : "http://ec2-3-26-10-163.ap-southeast-2.compute.amazonaws.com",
     withCredentials : true
 })
